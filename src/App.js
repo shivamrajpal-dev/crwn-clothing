62cf3fe3ef7c05555c270e6ca6c5e949a40bda31
shivam <shivam@shivams-MBP.domain.name>
2020-11-21 15:19:39 +0530

    broken our homepage into smaller components, finished styling our menu-item comopnent

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import HomePage from "./homepage.component.jsx";
+import HomePage from "./pages/homepage/homepage.component";
+import "./App.css";
 function App() {
   return (
     <div className="App">
