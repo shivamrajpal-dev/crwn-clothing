60ffbbc0e0b04ffc17a4f6b177b676049c7763b6
shivam <shivam@shivams-MBP.domain.name>
2020-11-27 13:49:19 +0530

    [D[D[D[D[C[C[C

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage.component";
 import "./App.css";
 import ShopPage from "./pages/shop/shop.component";
-
+import Header from "./components/header-component/header.component";
 function App() {
   return (
     <div>
       <BrowserRouter>
+        <Header />
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
